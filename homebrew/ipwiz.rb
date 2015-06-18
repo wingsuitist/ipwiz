ce34acb03180db36e7fb598db353e58d64c4c3ec
Jonas Felix <jonas.felix@felixideas.com>
2015-06-18 08:03:40 +0200

    adds homebrew formula

---
+++ homebrew/ipwiz.rb
@@ -0,0 +1,34 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Ipwiz < Formula
+  desc "find everything you need about an ip - nslookup, whois, dig..."
+  homepage "https://github.com/wingsuitist/ipwiz"
+  url "https://github.com/wingsuitist/ipwiz.git"
+  version "1.0.0"
+  sha256 ""
+
+  # depends_on "cmake" => :build
+  #depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    bin.install ['bin/ipwiz']
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test ipwiz`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
